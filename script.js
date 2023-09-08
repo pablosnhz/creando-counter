8adfd43dc60cea6dd56a9f6fa29ff8efa016e5a6
PabloSnhz <pablosanhuezaaa@gmail.com>
2023-09-08 16:34:35 -0300

    finalizado

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ let increment = 0;
 
 document.addEventListener('click', e => {
     // aumentar y decrementar
-    if (e.target.matches('.incrementar')) {
+    if (e.target.matches('.incremento')) {
         increment++;
     } else if (e.target.matches('.decremento')) {
         increment--;
     } else if (e.target.matches('.reset')) {
-        increment= 0;
+        increment = 0;
     }
+
+    show.textContent = increment;
 });
